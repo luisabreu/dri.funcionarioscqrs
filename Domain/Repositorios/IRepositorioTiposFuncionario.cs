fef87d886245564cd56367f0539ca6c1312d3c9d
luisabreu <labreu@gmail.com>
2015-02-10 16:19:40 +0000

    * added repository for tipos funcionarios

---
+++ Domain/Repositorios/IRepositorioTiposFuncionario.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Domain.Agregados.Entidades;
+
+namespace Domain.Repositorios {
+    public class IRepositorioTiposFuncionario {
+        private IEnumerable<TipoFuncionario> ObtemTiposFuncionario() {
+            return new[] {new TipoFuncionario(1, "Docente"), new TipoFuncionario(2, "Normal")};
+        }
+    }
+}
